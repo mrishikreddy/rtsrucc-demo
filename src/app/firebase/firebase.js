7761158875629274ecf15d05628bc4018e8ae198
mrishikreddy <malerishikreddy@gmail.com>
2025-08-31 10:31:10 +0530

    v2.0

--- src/app/firebase/firebase.js
+++ src/app/firebase/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyA9StoBQizKwsn6Gn9Znrr_4rzRd6g9g1I",
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
